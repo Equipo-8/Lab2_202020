50e06c5fafeb14324ad9c1106813bd46740ec81d
Juan Camacho <jm.camachop1@uniandes.edu.co>
2020-08-27 20:56:25 -0500

    Create app2.py

---
+++ App/app2.py
@@ -0,0 +1,42 @@
+import config as cf
+import sys
+import csv
+from ADT import list as lt
+from DataStructures import listiterator as it
+from DataStructures import liststructure as lt
+
+from time import process_time 
+
+
+def loadCSVFile (file, sep=";"):
+    """
+    Carga un archivo csv a una lista
+    Args:
+        file
+            Archivo csv del cual se importaran los datos
+        sep = ";"
+            Separador utilizado para determinar cada objeto dentro del archivo
+        Try:
+        Intenta cargar el archivo CSV a la lista que se le pasa por parametro, si encuentra algun error
+        Borra la lista e informa al usuario
+    Returns: None  
+    """
+    lst = lt.newList("ARRAY_LIST") #Usando implementacion arraylist
+    #lst = lt.newList() #Usando implementacion linkedlist
+    print("Cargando archivo ....")
+    t1_start = process_time() #tiempo inicial
+    dialect = csv.excel()
+    dialect.delimiter=sep
+    try:
+        with open(file, encoding="utf-8") as csvfile:
+            spamreader = csv.DictReader(csvfile, dialect=dialect)
+            for row in spamreader: 
+                lt.addLast(lst,row)
+    except:
+        print("Hubo un error con la carga del archivo")
+    t1_stop = process_time() #tiempo final
+    print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
+    return lst
+
+def encontrar_buenas_peliculas(peliculas,director,casting):
+    lt.
